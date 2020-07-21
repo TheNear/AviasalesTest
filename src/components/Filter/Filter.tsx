6aa74348439a12e01b5c65d7ca40421e44e2a40e
TheNear <thenearx@gmail.com>
2020-07-21 21:41:03 +0300

    create project structure

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Filter.module.css";
+
+function Filter(): JSX.Element {
+  return <div />;
+}
+
+export default Filter;
---
+++ src/components/Ticket/Ticket.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Ticket(): JSX.Element {
+  return <div />;
+}
+
+export default Ticket;
