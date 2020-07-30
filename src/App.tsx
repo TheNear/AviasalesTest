d1cd55421ab4c805f2ab08fb56ba777aa71db15f
TheNear <thenearx@gmail.com>
2020-07-30 21:07:42 +0300

    continue html

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,84 @@ function App(): JSX.Element {
       <main className="content-wrapper">
         <div className="filter">
           <div className="filter-inner">
-            21323sjadjaskdbasjkdjkashdjkshadjkhasjkdhjsadhjksahdjk
+            <h3 className="filter__title">Количество пересадок</h3>
+            <ul className="filter__list">
+              <li className="filter__item">
+                <label
+                  className="filter__item__label"
+                  htmlFor="filter__all-transfer"
+                >
+                  <input
+                    type="checkbox"
+                    className="filter__item__input"
+                    id="filter__all-transfer"
+                    name="filter__all-transfer"
+                    value="all-transfer"
+                  />
+                  Все
+                </label>
+              </li>
+              <li className="filter__item">
+                <label
+                  className="filter__item__label"
+                  htmlFor="filter__without-transfer"
+                >
+                  <input
+                    type="checkbox"
+                    className="filter__item__input"
+                    id="filter__without-transfer"
+                    name="filter__without-transfer"
+                    value="without-transfer"
+                  />
+                  Без пересадок
+                </label>
+              </li>
+              <li className="filter__item">
+                <label
+                  className="filter__item__label"
+                  htmlFor="filter__one-transfer"
+                >
+                  <input
+                    type="checkbox"
+                    className="filter__item__input"
+                    id="filter__one-transfer"
+                    name="filter__one-transfer"
+                    value="one-transfer"
+                  />
+                  1 пересадка
+                </label>
+              </li>
+              <li className="filter__item">
+                <label
+                  className="filter__item__label"
+                  htmlFor="filter__two-transfer"
+                >
+                  <input
+                    type="checkbox"
+                    className="filter__item__input"
+                    id="filter__two-transfer"
+                    name="filter__two-transfer"
+                    value="two-transfer"
+                  />
+                  2 пересадки
+                </label>
+              </li>
+              <li className="filter__item">
+                <label
+                  className="filter__item__label"
+                  htmlFor="filter__three-transfer"
+                >
+                  <input
+                    type="checkbox"
+                    className="filter__item__input"
+                    id="filter__three-transfer"
+                    name="filter__three-transfer"
+                    value="three-transfer"
+                  />
+                  3 пересадки
+                </label>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="sort">
