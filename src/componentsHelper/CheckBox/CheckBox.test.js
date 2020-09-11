be46582ecd85804f0f9a3d0d2cc3a7fbba80009f
TheNear <thenearx@gmail.com>
2020-09-11 23:39:30 +0300

    learn tests

--- src/componentsHelper/CheckBox/CheckBox.test.js
+++ src/componentsHelper/CheckBox/CheckBox.test.js
@@ -2,14 +2,21 @@ import React from "react";
 import { shallow } from "enzyme";
 import CheckBox from "./CheckBox";
 
+const getCheckBox = (props) => shallow(<CheckBox {...props}/>);
+
+const mockProps = {
+  text: "Check Box Title",
+  name: "check-box-name",
+}
+
 describe("Common Checkbox render", () => {
   it("Render empty Checkbox", () => {
-    const checkbox = shallow(<CheckBox />);
+    const checkbox = getCheckBox();
     expect(checkbox).toMatchSnapshot();
   });
 
   it("Render Checkbox with props", () => {
-    const checkbox = shallow(<CheckBox text="Check Box Title" name="check-box-name" />);
+    const checkbox = getCheckBox(mockProps);
     expect(checkbox).toMatchSnapshot();
   });
 });
