22131575b532bfcb4fb9cd1d1ed573be398c903b
TheNear <thenearx@gmail.com>
2020-09-13 16:04:39 +0300

    fixed setting with typescript jest coverage

--- src/componentsHelper/CheckBox/CheckBox.test.js
+++ src/componentsHelper/CheckBox/CheckBox.test.js
@@ -5,13 +5,11 @@ import CheckBox from "./CheckBox";
 describe("Common Checkbox render", () => {
   it("Render empty Checkbox", () => {
     const checkbox = shallow(<CheckBox />);
-    console.log(checkbox.debug());
     expect(checkbox).toMatchSnapshot();
   });
 
   it("Render Checkbox with props", () => {
     const checkbox = shallow(<CheckBox text="Check Box Title" name="check-box-name" />);
-    console.log(checkbox.debug())
     expect(checkbox).toMatchSnapshot();
   });
 });
