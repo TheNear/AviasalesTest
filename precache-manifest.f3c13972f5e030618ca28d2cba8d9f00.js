d72541d03676923b7899aa4064e6c81280389a91
TheNear <thenearx@gmail.com>
2020-10-27 20:11:24 +0300

    Updates

---
+++ precache-manifest.f3c13972f5e030618ca28d2cba8d9f00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fbc0335021c81f2b3352e763f511ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f01e1624356058a1f61",
+    "url": "/static/css/main.9ffe23c4.chunk.css"
+  },
+  {
+    "revision": "78e9bcdf2786cbf15dc9",
+    "url": "/static/js/2.ba0600f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba0600f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f01e1624356058a1f61",
+    "url": "/static/js/main.38138c42.chunk.js"
+  },
+  {
+    "revision": "45bb7c3d7e1fb529f677",
+    "url": "/static/js/runtime-main.f392bf54.js"
+  },
+  {
+    "revision": "c4580fba0b2ba9fe58191deed290b701",
+    "url": "/static/media/logo.c4580fba.svg"
+  },
+  {
+    "revision": "f7f06c5273c234ca3185bdfd5669d6e7",
+    "url": "/static/media/spinner.f7f06c52.svg"
+  }
+]);
